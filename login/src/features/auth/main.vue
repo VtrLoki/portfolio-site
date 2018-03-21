291045fd793d3efea29b0c883e2d7be8421f1bf2
VtrLoki <victor_vtr9@hotmail.com>
2018-03-20 21:18:27 -0300

    New components.

---
+++ login/src/features/auth/main.vue
@@ -0,0 +1,32 @@
+<script>
+
+  import SignIn from './sign-in'
+  import SignUp from './sign-up'
+  export default {
+    components: {
+      SignIn,
+      SignUp
+    }    
+  }
+
+</script>
+
+
+<template>
+  <div>
+    <div class="login-wrap">
+      <div class="login-html">
+        <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
+        <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+        <div class="login-form">
+					<sign-in/>
+					<sign-up/>	
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src="./style.css"></style>
+
+
